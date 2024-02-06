9c0895277d50ffbe12211161448c1e11bd297bb3
Andrea Sanchez <andreacsanchezb@gmail.com>
2024-02-06 13:31:58 +0100

    recycling principle ready for zimbabwe

--- holpa_master.R
+++ holpa_master.R
@@ -4,6 +4,25 @@ library(tidyverse)
 library(readxl)
 library(dplyr)
 
+##Country databases
+# Completed surveys
+zimbabwe_survey <- read_excel(
+  "C:/Users/andreasanchez/OneDrive - CGIAR/Bioversity/AI/HOLPA/analysis/HOLPA/zimbabwe_household_name_2023.11.27.xlsx",
+  sheet = "Final HOLPA_Zimbabwe_Household") %>%
+  rename("kobo_id"="_id")%>%
+  rename("country"="_1_2_1_3")
+
+# Country forms
+zimbabwe_choices <- read_excel(
+  "C:/Users/andreasanchez/OneDrive - CGIAR/Bioversity/AI/HOLPA/analysis/HOLPA/zimbabwe_household_form.xlsx",
+  sheet = "choices")%>%
+  select("list_name","name","label::English ((en))","score_agroecology_module")%>%
+  mutate(country="zimbabwe")%>%
+  rename("label_choice" = "label::English ((en))")%>%
+  rename("name_choice" = "name")
+
+
+#### Global databases
 global_form <- read_excel(
   "C:/Users/andreasanchez/OneDrive - CGIAR/Bioversity/AI/HOLPA/analysis/HOLPA/HOLPA_global_household_survey_20231204_mapped_to_indicators.xlsx",
                    sheet = "survey")%>%
@@ -29,57 +48,73 @@ global_choices <- read_excel(
   sheet = "choices")%>%
   select("list_name","name","label::English ((en))","score_agroecology_module")%>%
   rename("label_choice" = "label::English ((en))")%>%
-  rename("name_choice" = "name")
-
+  rename("name_choice" = "name")%>%
+  mutate(country="global")%>%
+  #Rbind country specific choices 
+  rbind(zimbabwe_choices)%>%
+  distinct(list_name,name_choice,label_choice, .keep_all = TRUE)%>%
+  left_join(global_form,by="list_name")
   
+
+ 
 ######Agroecology module
-agroecology<-  global_form%>%
+agroecology_form<-  global_form%>%
   filter(str_detect(module, "agroecology"))%>%
   select(-subindicator)%>%
   mutate(module= "agroecology")%>%
   mutate(indicator=if_else(str_detect(indicator, "1_recycling"),"1_recycling",
                            if_else(str_detect(indicator, "2_input_reduction"),"2_input_reduction",
                                    indicator)))
-  left_join(global_choices,by= "list_name")
 
-
+agrocology_choices<-global_choices%>%
+  filter(str_detect(module, "agroecology"))%>%
+  select(-subindicator)%>%
+  mutate(module= "agroecology")%>%
+  mutate(indicator=if_else(str_detect(indicator, "1_recycling"),"1_recycling",
+                           if_else(str_detect(indicator, "2_input_reduction"),"2_input_reduction",
+                                   indicator)))%>%
+  mutate(name_question= if_else(type_q=="select_multiple",
+                                paste(name_question,"/",name_choice, sep=""),
+                                name_question))
+  
 
 ## 1- Recycling structure
-recycling<- agroecology%>%
+recycling<- agrocology_choices%>%
   filter(str_detect(indicator, "1_recycling"))%>%
   select(label_question, name_question)%>%
+  distinct(., .keep_all = TRUE)%>%
   spread(key = name_question, value = label_question)%>%
   mutate("kobo_id"="kobo_farmer_id",
          "country"="country_name")
 sort(unique(recycling$type_q))  
 
 ## 2- Input reduction structure
-input_reduction<- agroecology%>%
+input_reduction<- agrocology_choices%>%
   filter(str_detect(indicator, "2_input_reduction"))%>%
-  left_join(global_choices,by= "list_name")%>%
-  mutate(name_question= if_else(type_q=="select_multiple",
-                                paste(name_question,"/",name_choice, sep=""),
-                                name_question))%>%
   dplyr::select(label_question, name_question)%>%
   distinct(., .keep_all = TRUE)%>%
-  spread(key = name_question, value = label_question)
+  spread(key = name_question, value = label_question)%>%
+  mutate("kobo_id"="kobo_farmer_id",
+         "country"="country_name")
   
 sort(unique(input_reduction$type_q))  
 
 
 #####Prueba  
-zimbabwe_survey <- read_excel(
-    "C:/Users/andreasanchez/OneDrive - CGIAR/Bioversity/AI/HOLPA/analysis/HOLPA/zimbabwe_household_name_2023.11.27.xlsx",
-    sheet = "Final HOLPA_Zimbabwe_Household") %>%
-  rename("kobo_id"="_id")%>%
-  rename("country"="_1_2_1_3")
-
-
 recycling_columns <- colnames(recycling)
 
 recycling_zimbabwe <- select(zimbabwe_survey, all_of(recycling_columns))%>%
-  gather(key = "name_question", value = "name_answer",-kobo_id, -country)%>%
-  left_join(agroecology, by="name_question")
+  gather(key = "name_question", value = "name_choice",-kobo_id, -country)%>%
+  mutate(name_choice= as.character(name_choice))%>%
+  dplyr::left_join(agrocology_choices, by=c("name_question"="name_question",
+                                            "name_choice"="name_choice"))%>%
+  select("module", "indicator",country.x,"kobo_id","name_question", "type", "type_q"  , "list_name" , 
+         "label_question","label_choice", "name_choice" ,"score_agroecology_module")
+  
+  
+sort(unique(recycling_zimbabwe$name_choice))
+names(recycling_zimbabwe)  
+  
 
 length(unique(recycling_zimbabwe$kobo_id))
 improve_indicator <- function(row, col_0, col_1, col_2, col_3) {
@@ -96,6 +131,11 @@ improve_indicator <- function(row, col_0, col_1, col_2, col_3) {
 
 input_reduction_columns <- colnames(input_reduction)
 input_reduction_zimbabwe <- select(zimbabwe_survey, all_of(input_reduction_columns))%>%
+  gather(key = "name_question", value = "name_choice",-kobo_id, -country)%>%
+  dplyr::left_join(agrocology_choices, by=c("name_question"="name_question"))
+                                            
+
+  
   #_1_4_3_1:**Over the past 12 months [add country meaning], what did you do to improve the soil fertility of cropland?**
   mutate(
     new_column_soil_fertility = improve_indicator(c_across(starts_with("_1_4_3_1")), 
@@ -114,7 +154,5 @@ input_reduction_zimbabwe <- select(zimbabwe_survey, all_of(input_reduction_colum
                                                      if_else("_1_4_3_1/0"==0&"_1_4_3_1/1"==1&"_1_4_3_1/2"==1&"_1_4_3_1/3"==0,2,9))))))
                                                              
                     
-  gather(key = "name_question", value = "name_choice",-kobo_id, -country)%>%
-  left_join(agroecology, by="name_question")
 
 length(unique(recycling_zimbabwe$kobo_id))
